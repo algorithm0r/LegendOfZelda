9f0ac923ab9805916283942ae257e46e8be781cd
Chris Marriott <algorithm0r@gmail.com>
2026-01-14 21:58:57 -0800

    Delete components/randommovement.js

--- components/randommovement.js
+++
@@ -1,8 +0,0 @@
-// RandomMovement component - stores wandering AI behavior parameters
-class RandomMovement {
-    constructor(speed = 80, changeInterval = 1.5) {
-        this.speed = speed;                    // Movement speed in pixels/second
-        this.changeInterval = changeInterval;  // Seconds between direction changes
-        this.changeTimer = 0;                  // Accumulated time since last change
-    }
-}
